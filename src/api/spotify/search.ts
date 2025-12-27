b83529cfb7d97c6a7dfd62f6f6a328269ede2144
MrPandir <MrPandir@users.noreply.github.com>
2025-12-27 16:03:55 +0100

    fix: use GraphQL for Spotify lookups
    Use GraphQL to avoid REST 429 error. Switch Spotify lookups from REST to
    GraphQL and return Track entities, updating callers and URL handlers to
    accept Track objects.

--- src/api/spotify/search.ts
+++ src/api/spotify/search.ts
@@ -1,27 +1,66 @@
-import type {
-  SpotifySearchResponse,
-  SpotifyErrorResponse,
-  SpotifyTrack,
-} from "./types";
+import { Track } from "@entities";
+import type { SpotifySearchResponse, SpotifyErrorResponse } from "./types";
+
+const definitionSearchTracks = {
+  name: "searchTracks",
+  operation: "query",
+  sha256Hash:
+    "bc1ca2fcd0ba1013a0fc88e6cc4f190af501851e3dafd3e1ef85840297694428",
+  value: null,
+};
 
 export async function search(
   query: string,
   limit: number = 1,
-): Promise<SpotifySearchResponse | SpotifyErrorResponse> {
-  return Spicetify.CosmosAsync.get(
-    `https://api.spotify.com/v1/search?q=${query}&type=track&limit=${limit}`,
-  );
+): Promise<Track[] | null> {
+  const response: SpotifySearchResponse | SpotifyErrorResponse =
+    await Spicetify.CosmosAsync.get(
+      `https://api.spotify.com/v1/search?q=${query}&type=track&limit=${limit}`,
+    );
+
+  if (!("tracks" in response)) return null;
+
+  const items = response.tracks.items;
+  return items.map((item) => Track.fromSpotifyTrack(item));
 }
 
-export async function searchTrack(query: string): Promise<SpotifyTrack | null> {
-  const result = await search(query);
+export async function searchGraphQL(
+  query: string,
+  limit: number = 1,
+): Promise<Track[] | null> {
+  const response = await Spicetify.GraphQL.Request(
+    // HACK: Definitions.searchTracks may not be initialized
+    Spicetify.GraphQL.Definitions.searchTracks ?? definitionSearchTracks,
+    {
+      searchTerm: query,
+      limit: limit,
+      includeAudiobooks: false,
+      includeAuthors: false,
+      includePreReleases: false,
+    },
+  );
 
-  if (!("tracks" in result)) {
-    console.error(
-      `Failed to fetch track: ${result.code} ${result.message} with query "${query}"`,
+  // TODO: Add typing
+  const items: undefined | { item: any; matchedFields: [] }[] =
+    response?.data?.searchV2?.tracksV2?.items;
+
+  if (!items?.length) return null;
+
+  return items.map(({ item }) => {
+    const data = item.data;
+    return new Track(
+      data.uri,
+      data.name,
+      data.artists.items.map((item: any) => item.profile.name),
     );
-    return null;
-  }
+  });
+}
 
-  return result.tracks?.items?.[0];
+export async function searchTrack(query: string): Promise<Track | null> {
+  // NOTE: switched to using GraphQL, as the API kept returning a 429 error
+  const result = await searchGraphQL(query);
+
+  if (!result) return null;
+
+  return result[0];
 }
--- src/api/spotify/tracks.ts
+++ src/api/spotify/tracks.ts
@@ -1,4 +1,13 @@
+import { Track } from "@entities";
 import type { SpotifyTrack, SpotifyErrorResponse } from "./types";
+
+const definitionGetTrack = {
+  name: "getTrack",
+  operation: "query",
+  sha256Hash:
+    "612585ae06ba435ad26369870deaae23b5c8800a256cd8a57e08eddc25a37294",
+  value: null,
+};
 
 // TODO: Add ?market=from_token and check is_playable
 
@@ -8,10 +17,29 @@ async function _getTrack(
   return Spicetify.CosmosAsync.get(
     `https://api.spotify.com/v1/tracks/${trackId}`,
   );
+  // if (!("uri" in response)) return null;
+  // return response;
 }
 
-export async function getTrack(trackId: string): Promise<SpotifyTrack | null> {
-  const response = await _getTrack(trackId);
-  if (!("uri" in response)) return null;
-  return response;
+async function _getTrackGraphQL(uri: string): Promise<Track | null> {
+  const response = await Spicetify.GraphQL.Request(
+    // HACK: Definitions.getTrack may not be initialized
+    Spicetify.GraphQL.Definitions.getTrack ?? definitionGetTrack,
+    { uri: uri },
+  );
+
+  const data = response.data.trackUnion;
+
+  if (!("uri" in data)) return null;
+
+  const dataArtists: [any] = data.firstArtist.items.concat(
+    data.otherArtists.items,
+  );
+  const artists = dataArtists.map((artist) => artist.profile.name);
+
+  return new Track(data.uri, data.name, artists);
+}
+
+export async function getTrack(uri: Spicetify.URI): Promise<Track | null> {
+  return _getTrackGraphQL(uri.toString());
 }
