a673d501e6d6c647b2f6ff82c242a3e93d61e2b8
jefflima1 <jefflima1@users.noreply.github.com>
2026-01-06 21:42:19 +0100

    feat: add Brazilian Portuguese locale support

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -1,14 +1,17 @@
 import en from "./en";
 import ru from "./ru";
+import pt_br from "./pt_BR";
 
 export enum Language {
   EN = "EN",
   RU = "RU",
+  PT_BR = "pt_BR",
 }
 
 export const locales: Record<Language, LocaleDefinition> = {
   [Language.EN]: en,
   [Language.RU]: ru,
+  [Language.PT_BR]: pt_br,
 };
 
 export type LocaleDefinition = typeof en;
