43ed2eaa7e74950eb0e1811d95854d29362f5588
MrPandir <MrPandir@users.noreply.github.com>
2026-01-11 19:39:44 +0100

    fix(songlink): correct assembly of proxied API request

--- src/api/songlink/get.ts
+++ src/api/songlink/get.ts
@@ -1,15 +1,20 @@
 import type { SongLinkResponse } from "./types";
 
-const proxyUrl = "https://mr-cors-proxy.vercel.app/api/proxy?url=";
-const apiUrl = new URL(proxyUrl + "https://api.song.link/v1-alpha.1/links");
-apiUrl.searchParams.append("songIfSingle", "true");
-apiUrl.searchParams.append("type", "song");
+const PROXY_BASE = "https://mr-cors-proxy.vercel.app/api/proxy";
+const SONG_LINK_API_BASE = "https://api.song.link/v1-alpha.1/links";
 
 export async function getSongLink(
   url: string,
 ): Promise<SongLinkResponse | null> {
-  apiUrl.searchParams.set("url", encodeURIComponent(url));
-  const response = await fetch(apiUrl.toString());
+  const apiSongLinkUrl = new URL(SONG_LINK_API_BASE);
+  apiSongLinkUrl.searchParams.set("url", url);
+  apiSongLinkUrl.searchParams.set("songIfSingle", "true");
+  apiSongLinkUrl.searchParams.set("type", "song");
+
+  const proxyUrl = new URL(PROXY_BASE);
+  proxyUrl.searchParams.set("url", apiSongLinkUrl.toString());
+
+  const response = await fetch(proxyUrl.toString());
 
   if (!response.ok) return null;
 
