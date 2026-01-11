33e6a84b7d0584e33729c5170c4617099d0d5ff8
MrPandir <MrPandir@users.noreply.github.com>
2026-01-11 17:00:05 +0100

    fix(songlink): replace the allorigins proxy with my own proxy server

--- src/api/songlink/get.ts
+++ src/api/songlink/get.ts
@@ -1,6 +1,6 @@
 import type { SongLinkResponse } from "./types";
 
-const proxyUrl = "https://api.allorigins.win/get?url=";
+const proxyUrl = "https://mr-cors-proxy.vercel.app/api/proxy?url=";
 const apiUrl = new URL(proxyUrl + "https://api.song.link/v1-alpha.1/links");
 apiUrl.searchParams.append("songIfSingle", "true");
 apiUrl.searchParams.append("type", "song");
@@ -8,12 +8,10 @@ apiUrl.searchParams.append("type", "song");
 export async function getSongLink(
   url: string,
 ): Promise<SongLinkResponse | null> {
-  const targetUrl = `https://api.song.link/v1-alpha.1/links?songIfSingle=true&type=song&url=${encodeURIComponent(url)}`;
-  const proxyUrl = `https://api.allorigins.win/get?url=${encodeURIComponent(targetUrl)}`;
-  const response = await fetch(proxyUrl);
+  apiUrl.searchParams.set("url", encodeURIComponent(url));
+  const response = await fetch(apiUrl.toString());
 
   if (!response.ok) return null;
 
-  const data = await response.json();
-  return JSON.parse(data.contents);
+  return await response.json();
 }
